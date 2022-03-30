eacce2beacff59badc8f779fa4f6828936af1626
Roderick <iroderick@outlook.es>
2022-03-30 20:06:43 -0300

    TP01

---
+++ tp01.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+
+typedef int item;
+struct Nodo {
+    item dato;
+    struct Nodo* siguiente;
+}; typedef struct Nodo* Lista;
+
+Lista crearLista();
+
+// Lista esListaVacia(L);
+
+int main(){
+
+}
+
+Lista crearLista(){
+    return NULL;
+}
+
+// Lista esListaVacia(L){
+//     if (L == NULL)
+//     {
+//         return true;
+//     }else{
+//         return false;
+//     }
+    
+}
